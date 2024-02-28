6c0ac71b5b7d6149d5f88aca74c26671edd42ed4
Vilius <vilius.valinskis@energyadvice.lt>
2024-02-28 23:51:04 +0200

    Works

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0)
+  const [values, setValues] = useState([1, 2])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Count {count}</Text>
+      <Button title="Increment" onPress={() => setCount(count + 1)} />
+      {/*value table*/}
+        <Text>Values</Text>
+        <Button title="Add Value" onPress={() => setValues([...values, values.length + 1])} />
+        <Button title="Remove Value" onPress={() => setValues(values.slice(0, -1))} />
+        <View>
+          {values.map((value, index) => <Text key={index}>{value}</Text>)}
+        </View>
       <StatusBar style="auto" />
     </View>
   );
